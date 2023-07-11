1a18b96dbb6e0215ca3863ea155a77e62caabf92
asmimore <35525461+asmimore@users.noreply.github.com>
2023-07-11 17:22:47 +0530

    1st commit
    1st commit

---
+++ main.tf
@@ -0,0 +1,102 @@
+locals {
+  vpc_id              = "vpc-01f693da897bef531"
+  private_a_subnet_id = "subnet-00f517dc93aa5bbee"
+  private_b_subnet_id = "subnet-0ffcbd11b0352fa50"
+  public_a_subnet_id  = "subnet-0483d063d1fd92d8d"
+  public_b_subnet_id  = "subnet-0878d3dd4b55ed856"
+
+  ubuntu_ami = "ami-03f65b8614a860c29"
+
+  traffic_dist_map = {
+    blue = {
+      blue  = 100
+      green = 0
+    }
+    blue-90 = {
+      blue  = 90
+      green = 10
+    }
+    split = {
+      blue  = 50
+      green = 50
+    }
+    green-90 = {
+      blue  = 10
+      green = 90
+    }
+    green = {
+      blue  = 0
+      green = 100
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+# Security Group
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group
+resource "aws_security_group" "web" {
+  name        = "web-sg"
+  description = "Security group for web-servers with HTTP ports open within VPC"
+  vpc_id      = local.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+# Application Load Balancer
+# At least two subnets in two different Availability Zones must be specified
+# aws_lb: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
+resource "aws_lb" "app" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets = [
+    local.public_a_subnet_id,
+    local.public_b_subnet_id
+  ]
+  security_groups = [aws_security_group.web.id]
+}
+
+# Load Balancer Listener
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
+resource "aws_lb_listener" "app" {
+  load_balancer_arn = aws_lb.app.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "forward"
+    # target_group_arn = aws_lb_target_group.blue.arn
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
+      }
+
+      target_group {
+        arn    = aws_lb_target_group.green.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
+      }
+
+      stickiness {
+        enabled  = false
+        duration = 1
+      }
+    }
+  }
+}
